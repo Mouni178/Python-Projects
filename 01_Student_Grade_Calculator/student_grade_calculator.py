03e3ae9426ae879d2663d878254ae834f2ef68e8
Mounika <potthalammounika17@gmail.com>
2026-09-02 13:11:30 +0530

    Implement grade calculation and input handling
    Fix grade calculation logic and variable names.

---
+++ 01_Student_Grade_Calculator/student_grade_calculator.py
@@ -0,0 +1,24 @@
+def calculate_grade(average):
+  if average>=90:
+    return "A+"
+  elif average>=80:
+    return "A"
+  elif average>=70:
+    return "B"
+  elif average>=60:
+    return "C"
+  elif averge>=50:
+    return "D"
+  else:
+    return "F"
+name=input("Enter the Student Name : ")
+maths=float(input("Enter Maths Marks : ")
+english=float(input("Enter English Marks :")
+Social=float(input("Enter Social MArks :")
+total=maths+english+social
+average=total//3
+grade=calculate_grage(average)
+print("Name:",name)
+print("Total Marks:",total)
+print("Average:", round(average,2))
+print("Grade:",grade)
