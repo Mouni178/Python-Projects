9e4eabaefb8a78f5950c79b551a49eeba8744b08
Mounika <potthalammounika17@gmail.com>
2026-09-03 12:06:43 +0530

    Update number_guessing_game.py

--- 02_Number_Guessing_Game/number_guessing_game.py
+++ 02_Number_Guessing_Game/number_guessing_game.py
@@ -1,1 +1,17 @@
-
+import random
+print("Number Guessing Game")
+print("i have selected number from 1 to 100")
+number=random.randint(1,100)
+attempts=0
+while True:
+  guess=int(input("Enter a number:"))
+  attempts+=1
+  if guess<number:
+    print("Too Low! Try Again")
+  elif guess>number:
+    print("Too High! Try Again")
+  else:
+    print("Correct")
+    print("You guessed the number in,"attempts",attempts")
+    break
+  
