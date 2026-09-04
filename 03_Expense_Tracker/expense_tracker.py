c35c083ff2940d86dd0b6e361d913331a7d1602b
Mounika <potthalammounika17@gmail.com>
2026-09-04 17:13:18 +0530

    Update expense_tracker.py

--- 03_Expense_Tracker/expense_tracker.py
+++ 03_Expense_Tracker/expense_tracker.py
@@ -5,4 +5,34 @@ def add_expenses():
   expenses.append(expense)
   amounts.append(amount)
   print("Expense added Successfully")
-  
+def view_expenses():
+    if len(expenses) == 0:
+        print("No expenses found.")
+    else:
+        print("Your Expenses")
+        for i in range(len(expenses)):
+            print(i + 1, expenses[i],amounts[i],"rupees")
+def total_expenses():
+    total = 0
+    for amount in amounts:
+        total = total + amount
+    print("Total Expenses: ₹", total)
+while True:
+    print("1.add Expense")
+    print("2.View Expenses")
+    print("3.Total Expenses")
+    print("4.Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        add_expense()
+    elif choice == "2":
+        view_expenses()
+    elif choice == "3":
+        total_expenses()
+    elif choice == "4":
+        print("Thank you!")
+        break
+    else:
+        print("Invalid choice!")
+
+
