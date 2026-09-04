8f7d001f258a8c48fd2ade721173b8cbcbeddddb
Mounika <potthalammounika17@gmail.com>
2026-09-04 17:10:52 +0530

    Update expense_tracker.py

--- 03_Expense_Tracker/expense_tracker.py
+++ 03_Expense_Tracker/expense_tracker.py
@@ -1,1 +1,8 @@
-
+expenses=[]
+def add_expenses():
+  description=input("Enter Expense name:")
+  amount=float(input("Enter amount:")
+  expenses.append(expense)
+  amounts.append(amount)
+  print("Expense added Successfully")
+  
