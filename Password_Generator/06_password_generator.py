d175faa99c2f68e174530314ccefa86fa3ef229e
Mounika <potthalammounika17@gmail.com>
2026-09-09 21:06:22 +0530

    Update 06_password_generator.py

--- Password_Generator/06_password_generator.py
+++ Password_Generator/06_password_generator.py
@@ -1,1 +1,11 @@
-
+import random
+import string
+def generate_password(length):
+    characters = string.ascii_letters + string.digits
+    password = ""
+    for i in range(length):
+        password += random.choice(characters)
+    return password
+length = int(input("Enter password length: ")
+password = generate_password(length)
+print("Generated Password:", password)
