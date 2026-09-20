17fd460cc56b1fa7a23bfe01e862f0707a34ff99
Mounika <potthalammounika17@gmail.com>
2026-09-20 20:01:11 +0530

    Update word_counter.py

--- 13_Word_Counter/word_counter.py
+++ 13_Word_Counter/word_counter.py
@@ -1,1 +1,17 @@
-
+text = input("Enter a sentence: ")
+words = text.split()
+word_count = len(words)
+characters = len(text)
+print("TEXT ANALYSIS")
+print("Total words:", word_count)
+print("Total characters:", characters)
+frequency = {}
+for word in words:
+    word = word.lower()
+    if word in frequency:
+        frequency[word] += 1
+    else:
+        frequency[word] = 1
+print("WORD FREQUENCY")
+for word, count in frequency.items():
+    print(word, ":", count)
